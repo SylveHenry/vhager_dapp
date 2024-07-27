881d7d69b778e9dd2499acdb482d30917bba6105
lordace-coder <lordyacey@gmail.com>
2024-07-27 13:00:05 +0100

    attempt two

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Buffer } from "buffer";
+globalThis.Buffer = Buffer;
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ChakraProvider>
     <React.StrictMode>
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+// import { ProvidePlugin } from 'webpack';
+
+// export const resolve = {
+//   fallback: {
+//     "buffer": require.resolve("buffer/"),
+//   },
+// };
+// export const plugins = [
+//   new ProvidePlugin({
+//     Buffer: ['buffer', 'Buffer'],
+//   }),
+// ];
